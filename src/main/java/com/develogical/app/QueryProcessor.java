924c42d6acfef7dc15456df148824f7593933e53
ntazana <ntazana@gmail.com>
2019-04-17 18:07:23 +0200

    Saved a few typos

--- src/main/java/com/develogical/app/QueryProcessor.java
+++ src/main/java/com/develogical/app/QueryProcessor.java
@@ -3,7 +3,7 @@ package com.develogical.app;
 public class QueryProcessor {
     public String process(String query) {
         if (query.toLowerCase().contains("ntazana")) {
-            return "Ntazana is a tech start-up enterprenure and an avid AI advocate" +
+            return "Ntazana is a tech start-up enterprenure and an avid AI advocate. " +
                     "He graduated from Oxford in 2020ans setup Altum technica, Africa's largest tech company " +
                     "In 2022, Altum Technica was listed on the New York stock exchange, making it the only second African start up to feature.";
         }
